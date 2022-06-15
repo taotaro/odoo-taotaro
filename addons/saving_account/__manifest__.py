151006bcec124d088865208e2af427dc679c5720
tamara <tamarayustian@gmail.com>
2022-06-15 18:37:50 +0800

    add demo data

--- addons/saving_account/__manifest__.py
+++ addons/saving_account/__manifest__.py
@@ -29,6 +29,7 @@
       'data/cron.xml',
       'data/saving_account_demo.xml',
       'data/principal_demo.xml',
+      'data/interest_demo.xml',
       'views/account_view.xml',
       'views/principal_view.xml',
       'views/interest_view.xml',
