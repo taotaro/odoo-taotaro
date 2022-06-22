48ea0639e3c48286afd203ea4200970ce69de5b5
tamara <tamarayustian@gmail.com>
2022-06-22 16:35:39 +0800

    add interest rate model

---
+++ addons/saving_account/models/rate.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api
+
+class InterestRate(models.Model):
+    _name = 'interest_rate'
+    _description = 'Interest Rate Setup'
+
+    start_date = fields.Date(string='Starting Date', default=fields.Date.today())
+    account_type = fields.Selection([
+      ('normal', 'Normal'), 
+      ('vip', 'VIP')
+    ])
+    annual_rate = fields.Float(string='Annual Interest Rate')
