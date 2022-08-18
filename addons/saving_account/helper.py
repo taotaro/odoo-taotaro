dbf03787b2ee9969b4c3ea43b6c86e7b251dc56b
tamara <tamarayustian@gmail.com>
2022-08-18 17:15:33 +0800

    adjust truncate function

--- addons/saving_account/helper.py
+++ addons/saving_account/helper.py
@@ -1,5 +1,6 @@
 def truncate_number(f_number, n_decimals):
   strFormNum = "{0:." + str(n_decimals+5) + "f}"
   trunc_num = float(strFormNum.format(f_number)[:-5])
-  trunc_num = '%.2f' % trunc_num
+  trunc_value = '%.' + str(n_decimals) + 'f'
+  trunc_num = trunc_value % trunc_num
   return(trunc_num)
