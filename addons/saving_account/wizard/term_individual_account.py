6af303e01179a65a2c12354bc8fab0a9f6c0393b
kavyagb22 <kgbalaji@connect.ust.hk>
2023-05-17 22:20:07 +0800

    debug updated

--- addons/saving_account/wizard/term_individual_account.py
+++ addons/saving_account/wizard/term_individual_account.py
@@ -333,7 +333,8 @@ class TermIndividualAccountWizard(models.TransientModel):
   #   return
 
   def action_send_all_emails(self):
-    _logger.debug('this is a test message')
+    # _logger.debug('this is a test message')
+    _logger.info('this is a test message')
     account_ids = self.env['saving_account'].search([])
     report_id_ref = self.env.ref('saving_account.action_term_individual_account_report')
     email_to_send = self.env['email_setup'].search([], limit=1, order='create_date desc').email_to
