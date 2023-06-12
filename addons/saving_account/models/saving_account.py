6f46f9ed15fa6babae83241510f083caca8e3dd4
kavyagb22 <kgbalaji@connect.ust.hk>
2023-06-12 13:09:31 +0800

    testing total principal

--- addons/saving_account/models/saving_account.py
+++ addons/saving_account/models/saving_account.py
@@ -71,7 +71,7 @@ class SavingAccount(models.Model):
       # _logger.info(f'close date of rec: {rec.close_date}')
       if rec.close_date != False:
         _logger.info(f'closed account!: {rec.total_interest}')
-        rec.total_interest = rec.total_interest
+        # rec.total_interest = rec.total_interest
       # search interest entries of account
       interest_list = rec.env['saving_account.entry'].search([
         ('account_id','=',rec.id),
