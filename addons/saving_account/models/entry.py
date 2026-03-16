b88c8451bae23c4b19c2a3597436ba83d9ee4962
manichoy <56984167+manichoy@users.noreply.github.com>
2026-03-16 11:41:19 +0800

    Refactor create method for entry model

--- addons/saving_account/models/entry.py
+++ addons/saving_account/models/entry.py
@@ -89,30 +89,30 @@ class SavingAccountEntry(models.Model):
 
   @api.model
   def create(self, vals_list):
-  print("calling create")
-  # fill entry type field if there is any special conditions
+    print("calling create")
+    # fill entry type field if there is any special conditions
     
-  for vals in vals_list:
+    for vals in vals_list:
     # 1) 取 entry_type_principal → 寫返入 entry_type
-    entry_type_principal = vals.get('entry_type_principal')
-    if entry_type_principal:
-        vals['entry_type'] = entry_type_principal
+      entry_type_principal = vals.get('entry_type_principal')
+      if entry_type_principal:
+          vals['entry_type'] = entry_type_principal
 
     # 2) fill reference number according to entry type
-    entry_type = vals.get('entry_type')
-    if entry_type == 'deposit':
+      entry_type = vals.get('entry_type')
+      if entry_type == 'deposit':
         vals['ref_no'] = 'DP'
         vals['ledger'] = 'principal'
-    elif entry_type == 'withdraw':
+      elif entry_type == 'withdraw':
         vals['ref_no'] = 'WD'
         vals['ledger'] = 'principal'
-    elif entry_type == 'credit_interest':
+      elif entry_type == 'credit_interest':
         vals['ref_no'] = 'CI'
   
     # 3) create unique id for each entry（保留你原本邏輯：每筆都取一次 sequence）
-    vals['entry_no'] = self.env['ir.sequence'].next_by_code('saving_account.entry')
+      vals['entry_no'] = self.env['ir.sequence'].next_by_code('saving_account.entry')
 
-  return super(SavingAccountEntry, self).create(vals)
+    return super(SavingAccountEntry, self).create(vals)
         
   # def create(self, vals):
   #   print("calling create")
