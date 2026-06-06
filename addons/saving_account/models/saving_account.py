2d7c2b20c519e8955b9386c8523f3430cb370802
manichoy <mani.choy@innocorpsl.com>
2026-06-06 23:22:49 +0800

    update saving account v19 update

--- addons/saving_account/models/saving_account.py
+++ addons/saving_account/models/saving_account.py
@@ -30,11 +30,20 @@ class SavingAccount(models.Model):
   custom1 = fields.Text(string='Custom 1')
   custom2 = fields.Text(string='Custom 2')
 
-  @api.model
-  def create(self, vals):
-    # create unique id for each account
-    vals['account_no'] = self.env['ir.sequence'].next_by_code('saving_account')
-    return super(SavingAccount, self).create(vals)
+
+  @api.model_create_multi
+  def create(self, vals_list):
+      for vals in vals_list:
+          if not vals.get('account_no'):
+              vals['account_no'] = self.env['ir.sequence'].next_by_code('saving_account') or '/'
+      return super().create(vals_list)
+
+
+  # @api.model
+  # def create(self, vals):
+  #   # create unique id for each account
+  #   vals['account_no'] = self.env['ir.sequence'].next_by_code('saving_account')
+  #   return super(SavingAccount, self).create(vals)
   
   # calculate total principal amount of each account
   @api.depends('principal_list_ids')
@@ -183,5 +192,4 @@ class SavingAccount(models.Model):
           'default_entry_type_principal': 'withdraw',
           'default_amount': account.total_principal + account.total_interest
         }
-      }
-    
+      } 
