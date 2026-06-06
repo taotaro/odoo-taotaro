b339d125a99d297197c0442c7471120dd78771c0
manichoy <mani.choy@innocorpsl.com>
2026-06-06 19:10:23 +0800

    Keep only report_wizard import during v19 upgrade

--- addons/saving_account/wizard/report_wizard.py
+++ addons/saving_account/wizard/report_wizard.py
@@ -1,6 +1,10 @@
 
-from odoo import models
+from odoo import models, fields
+
 
 class XTermIndividualAccountReportWizard(models.TransientModel):
     _name = "x_term_individual_account.report.wizard"
     _description = "Term Individual Account Report Wizard"
+    _rec_name = "create_date"
+
+    create_date = fields.Datetime(readonly=True)
