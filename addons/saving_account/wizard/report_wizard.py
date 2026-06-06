4df0129d76861d371df245b97d42ce642aa1aa5c
manichoy <mani.choy@innocorpsl.com>
2026-06-06 18:12:48 +0800

    Temporarily isolate report_wizard for v19 upgrade

--- addons/saving_account/wizard/report_wizard.py
+++ addons/saving_account/wizard/report_wizard.py
@@ -3,4 +3,4 @@ from odoo import models
 
 class XTermIndividualAccountReportWizard(models.TransientModel):
     _name = "x_term_individual_account.report.wizard"
-    _description = "Report Wizard"
+    _description = "Term Individual Account Report Wizard"
