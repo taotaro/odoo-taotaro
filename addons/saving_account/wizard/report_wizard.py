29122a3e746857a055c02c049aefc069515f3c19
manichoy <mani.choy@innocorpsl.com>
2026-06-06 17:51:12 +0800

    v19 upgrade step 1 fix saving_account

---
+++ addons/saving_account/wizard/report_wizard.py
@@ -0,0 +1,6 @@
+
+from odoo import models
+
+class XTermIndividualAccountReportWizard(models.TransientModel):
+    _name = "x_term_individual_account.report.wizard"
+    _description = "Report Wizard"
