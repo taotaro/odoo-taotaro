8c5392345a1d90b134d0fe1346633c765150a0d6
manichoy <mani.choy@innocorpsl.com>
2026-07-06 23:08:12 +0800

    Fix term individual account report email rendering

--- addons/saving_account/wizard/term_account.py
+++ addons/saving_account/wizard/term_account.py
@@ -2,138 +2,149 @@ from ..helper import truncate_number, find_last_1april
 from odoo import models, fields, api, _
 import base64
 import logging
+
 _logger = logging.getLogger(__name__)
 
 
 class TermAccountWizard(models.TransientModel):
-  _name="term_account.report.wizard"
-  _description="Print Term Account Report Wizard"
+  _name = "term_account.report.wizard"
+  _description = "Print Term Account Report Wizard"
 
   account_type = fields.Selection([
     ('all', 'All'),
-    ('normal', 'Normal'), 
+    ('normal', 'Normal'),
     ('vip', 'VIP')
   ], default='all', string="Account Type")
-  date_from=fields.Date(string="Date as of", default=fields.Date.today())
-  date_to=fields.Date(string="Date To")
-  email_to=fields.Char(string="Email To", _compute="_get_default_email")
 
-  @api.onchange('email_to')
-  def _get_default_email(self):
-    email_to_send = self.env['email_setup'].search([], limit=1, order='create_date desc').email_to
-    for rec in self:
-      rec.email_to = email_to_send
+  date_from = fields.Date(string="Date as of", default=fields.Date.today())
+  date_to = fields.Date(string="Date To")
+  email_to = fields.Char(string="Email To")
+
+  @api.model
+  def default_get(self, fields_list):
+    res = super().default_get(fields_list)
+
+    setup = self.env['email_setup'].search(
+      [],
+      limit=1,
+      order='create_date desc'
+    )
+
+    if setup:
+      res['email_to'] = setup.email_to
+
+    return res
 
   def generate_report(self):
     from_date = ""
     type_account = ""
+
     if not self.date_from:
       from_date = fields.Date.today()
     else:
       from_date = self.date_from
 
-    _logger.info(f'logger from date {from_date}')
+    _logger.info("Term account report date from: %s", from_date)
 
     if not self.account_type:
       type_account = 'all'
     else:
       type_account = self.account_type
 
-    #find last 1 april
     april = find_last_1april(from_date)
-    
-    # find accounts in specified term and types
+
     accounts = []
+
     account_401 = self.env['saving_account'].search_read([
       ('account_no', '=', '401')
     ])
-    _logger.info(f'found: account 401 {account_401}')
+    _logger.info("Found account 401: %s", account_401)
+
     if type_account != 'all':
       accounts = self.env['saving_account'].search_read([
-        ('account_type','=',type_account),
-        ('open_date','<=',from_date),
-        # ('close_date','=',False)
+        ('account_type', '=', type_account),
+        ('open_date', '<=', from_date),
       ])
     else:
       accounts = self.env['saving_account'].search_read([
-        ('open_date','<=',from_date),
-        # ('close_date','=',False)
+        ('open_date', '<=', from_date),
       ])
 
-    # truncate properly
     for account in accounts:
       close_date = account['close_date']
       account_no = account['account_no']
-      # _logger.info(f'account info: {account_no}: {close_date}')
-      
-      # account['total_principal'] = truncate_number(account['total_principal'], 2)
 
-     ###### ADD THIS BACK
-      # if account['close_date'] != False:
-      #   if account['close_date'] <= from_date:
-      #     _logger.info(f'greater: {account_no}: {close_date}')
-      #     account['last_interest_credit'] = 0
+      _logger.info(
+        "Processing account %s, close date: %s",
+        account_no,
+        close_date
+      )
 
-
-
-      # account['last_interest_credit'] = truncate_number(account['last_interest_credit'], 2)
-      credit_total = 0
       interest_credit = self.env['saving_account.entry'].search([
-        ('account_id','=',account['id']), 
-        ('entry_type','=','credit_interest'), 
-        ('ledger','=','principal'),
-        ('entry_date','<=',from_date)
-        ])
+        ('account_id', '=', account['id']),
+        ('entry_type', '=', 'credit_interest'),
+        ('ledger', '=', 'principal'),
+        ('entry_date', '<=', from_date)
+      ])
+
       if interest_credit:
         account['last_interest_credit'] = interest_credit[-1].amount
       else:
         account['last_interest_credit'] = 0.0
-      last_interest_credit = account['last_interest_credit']
-      _logger.info(f'credit total: {last_interest_credit} ')
 
-      # calculate balance upto given date
+      _logger.info(
+        "Last interest credit for account %s: %s",
+        account_no,
+        account['last_interest_credit']
+      )
+
       principal_list = self.env['saving_account.entry'].search([
-      ('account_id','=',account['id']), 
-      ('ledger','=','principal'),
-      ('entry_date','<=',from_date)
+        ('account_id', '=', account['id']),
+        ('ledger', '=', 'principal'),
+        ('entry_date', '<=', from_date)
       ])
+
       current_total = 0
+
       if principal_list:
         for principal in principal_list:
           if principal.entry_type == "deposit":
-            current_total = current_total + principal.amount
+            current_total += principal.amount
           elif principal.entry_type == "withdraw":
-            current_total = current_total - principal.amount
+            current_total -= principal.amount
           elif principal.entry_type == "credit_interest":
-            current_total = current_total + principal.amount
-      # _logger.info(f'principal current: {current_total}')
-      # _logger.info(f'total principal: {total_principal}')
+            current_total += principal.amount
+
       account['total_principal'] = truncate_number(current_total, 2)
 
-      # find total interest credit
       entries = self.env['saving_account.entry'].search_read([
-        ('account_id','=', account['id']),
-        ('entry_type','=','credit_interest'), 
-        ('ledger','=','principal'),
-        ('entry_date','>=',april), 
-        ('entry_date','<=',from_date)
+        ('account_id', '=', account['id']),
+        ('entry_type', '=', 'credit_interest'),
+        ('ledger', '=', 'principal'),
+        ('entry_date', '>=', april),
+        ('entry_date', '<=', from_date)
       ])
+
       total_interest_credit = 0
+
       for entry in entries:
-        acc = entry['account_no']
-        _logger.info(f'test entry: {acc}')
         total_interest_credit += entry['amount']
 
-      account['total_interest_credit'] = truncate_number(total_interest_credit, 2)
-    
+      account['total_interest_credit'] = truncate_number(
+        total_interest_credit,
+        2
+      )
+
     if self.read():
-      data = { 
+      data = {
         'form': self.read()[0],
         'accounts': accounts
       }
     else:
       data = {
-        'form': { 'date_from': from_date },
+        'form': {
+          'date_from': from_date
+        },
         'accounts': accounts
       }
 
@@ -141,32 +152,73 @@ class TermAccountWizard(models.TransientModel):
 
   def action_print_report(self):
     data = self.generate_report()
-    return self.env.ref('saving_account.action_term_account_report').report_action(self, data=data)
+
+    return self.env.ref(
+      'saving_account.action_term_account_report'
+    ).report_action(self, data=data)
 
   def action_send_email(self):
+    self.ensure_one()
+
     data = self.generate_report()
-    report_id = self.env.ref('saving_account.action_term_account_report')._render(self.ids, data)
-    report_b64 = base64.b64encode(report_id[0])
+
+    report = self.env.ref(
+      'saving_account.action_term_account_report'
+    )
+
+    pdf_content, report_type = report._render_qweb_pdf(
+      report.report_name,
+      res_ids=self.ids,
+      data=data
+    )
+
+    report_b64 = base64.b64encode(pdf_content)
+
     now = fields.Datetime.today().strftime('%Y%m%d')
     report_name = now + '_term_account.pdf'
-    
+
     attachment = self.env['ir.attachment'].create({
-            'name': report_name,
-            'type': 'binary',
-            'datas': report_b64,
-            'store_fname': report_name,
-            'mimetype': 'application/x-pdf'
-        })
+      'name': report_name,
+      'type': 'binary',
+      'datas': report_b64,
+      'mimetype': 'application/pdf',
+    })
 
-    email_to_send = self.env['email_setup'].search([], limit=1, order='create_date desc').email_to
-    email_values = {'email_to': email_to_send}
-    print("Sending email to", email_to_send)
+    email_to_send = self.email_to or self.env['email_setup'].search(
+      [],
+      limit=1,
+      order='create_date desc'
+    ).email_to
 
-    report_template_id = self.env.ref('saving_account.mail_template_term_account')
-    report_template_id.attachment_ids = [(6, 0, [attachment.id])]
+    if not email_to_send:
+      return {
+        'type': 'ir.actions.client',
+        'tag': 'display_notification',
+        'params': {
+          'title': _('Warning'),
+          'message': 'No email recipient configured.',
+          'sticky': True,
+        }
+      }
+
+    email_values = {
+      'email_to': email_to_send,
+      'attachment_ids': [(4, attachment.id)],
+    }
+
+    _logger.info("Sending term account email to %s", email_to_send)
+
+    report_template_id = self.env.ref(
+      'saving_account.mail_template_term_account'
+    )
+
     try:
-      # send confirmation message when successful
-      report_template_id.send_mail(self.id, email_values=email_values, force_send=True)
+      report_template_id.send_mail(
+        self.id,
+        email_values=email_values,
+        force_send=True
+      )
+
       return {
         'type': 'ir.actions.client',
         'tag': 'display_notification',
@@ -176,30 +228,30 @@ class TermAccountWizard(models.TransientModel):
           'sticky': True,
         }
       }
-    except:
-      # send warning message when fail
+
+    except Exception as e:
+      _logger.exception("Term account email send failed: %s", e)
+
       return {
-          'type': 'ir.actions.client',
-          'tag': 'display_notification',
-          'params': {
-            'title': _('Warning'),
-            'message': 'Email failed to send',
-            'sticky': True,
-          }
+        'type': 'ir.actions.client',
+        'tag': 'display_notification',
+        'params': {
+          'title': _('Warning'),
+          'message': 'Email failed to send: %s' % str(e),
+          'sticky': True,
+        }
       }
 
   @api.model
   def _cron_send_email(self):
     try:
-      print("Sending scheduled email...")
-      self.action_send_email()
-    except:
-      return {
-          'type': 'ir.actions.client',
-          'tag': 'display_notification',
-          'params': {
-            'title': _('Warning'),
-            'message': 'Email failed to send',
-            'sticky': True,
-          }
-      }
+      _logger.info("Sending scheduled term account email...")
+
+      wizard = self.create({
+        'date_from': fields.Date.today(),
+      })
+
+      wizard.action_send_email()
+
+    except Exception as e:
+      _logger.exception("Term Account Email Error: %s", e)
--- addons/saving_account/wizard/term_individual_account.py
+++ addons/saving_account/wizard/term_individual_account.py
@@ -1,159 +1,188 @@
-from datetime import datetime
 from odoo import models, fields, api, _
-import base64
-from ..helper import find_last_1april, find_last_1oct, truncate_number, find_date_from
+from odoo.exceptions import UserError
+from ..helper import truncate_number, find_date_from
+
 from collections import defaultdict
-import time
-# import threading
+import base64
 import logging
+
 _logger = logging.getLogger(__name__)
-seconds = 1672215379.5045543
-local_time = time.ctime(seconds)
+
 
 class TermIndividualAccountWizard(models.TransientModel):
-  _name="term_individual_account.report.wizard"
-  _description="Print Term Individual Account Report Wizard"
+  _name = "term_individual_account.report.wizard"
+  _description = "Print Term Individual Account Report Wizard"
 
   account_id = fields.Many2one('saving_account', string='Account')
-  # date_from=fields.Date(string="Date From", _compute='_compute_date_from', compute_sudo=True)
-  date_from=fields.Date(string="Date From")
-  date_to=fields.Date(string="Date To", default=fields.Date.today())
-  email_to=fields.Char(string="Email To", _compute="_get_default_email")
+  date_from = fields.Date(string="Date From")
+  date_to = fields.Date(string="Date To", default=fields.Date.today())
+  email_to = fields.Char(string="Email To")
 
-  # find the financial term start date (if date is after april and before sept, then start date is april, else october)
-  # @api.onchange('date_from')
-  # def _compute_date_from(self):
-  #   for rec in self:
-  #     from_date = fields.Date.today()
-  #     april = find_last_1april(from_date)
-  #     oct = find_last_1oct(from_date)
-  #     sept = datetime(year=april.year, month=9, day=30).date()
-  #     mar = datetime(year=oct.year+1, month=3, day=31).date()
-      
-  #     ### CHATGPT- rephrased to remove if else statement
-  #     if april <= from_date and from_date <= sept:
-  #       rec.date_from = april
-  #     elif oct <= from_date and from_date <= mar:
-  #       rec.date_from = oct
-      
-  # set email address
-  @api.onchange('email_to')
-  def _get_default_email(self):
-    email_to_send = self.env['email_setup'].search([], limit=1, order='create_date desc').email_to
-    for rec in self:
-      rec.email_to = email_to_send
+  @api.model
+  def default_get(self, fields_list):
+    res = super().default_get(fields_list)
 
- 
+    setup = self.env['email_setup'].search(
+      [],
+      limit=1,
+      order='create_date desc'
+    )
 
+    if setup:
+      res['email_to'] = setup.email_to
+
+    return res
 
   def generate_report(self, account_id=False):
     from_date = self.date_from or find_date_from()
     to_date = self.date_to or fields.Date.today()
     account_id = account_id or self.account_id
-        # find all entries for the account
+
+    if not account_id:
+      raise UserError(_("Please select an account."))
+
     all_entries = self.env['saving_account.entry'].search_read([
-        ('account_id','=', account_id.id), 
-        ('ledger','=','principal'), 
+      ('account_id', '=', account_id.id),
+      ('ledger', '=', 'principal'),
     ])
 
-    # partition entries into those within the specified date and those before it
     initial_balance = 0
     entries = []
     total_values = defaultdict(int)
 
-    # get initial balance and entries before specified date
     for entry in all_entries:
-     
       if from_date <= entry['entry_date'] <= to_date:
         entries.append(entry)
       elif entry['entry_date'] < from_date:
-        # initial_entries.append(entry)
         if entry['entry_type'] == 'withdraw':
           initial_balance -= entry['amount']
         else:
           initial_balance += entry['amount']
-   
+
     initial_entry = {
-        "entry_type": "initial",
-        "amount": 0,
-        "balance": initial_balance,
-        "create_date": from_date,
-        "ref_no": "BF"
+      "entry_type": "initial",
+      "amount": 0,
+      "balance": truncate_number(initial_balance, 2),
+      "create_date": from_date,
+      "entry_date": from_date,
+      "ref_no": "BF",
     }
+
     entries.insert(0, initial_entry)
 
-    total_values = defaultdict(int)
-    for entry in entries:
-        if entry['entry_type'] in {'withdraw', 'deposit', 'credit_interest'}:
-            amount = entry['amount']
-            if entry['entry_type'] == 'withdraw':
-                initial_balance -= amount
-            else:
-                initial_balance += amount
-            total_values[entry['entry_type']] += amount
-            entry['amount'] = truncate_number(amount, 2)
-            entry['balance'] = truncate_number(initial_balance, 2)
+    running_balance = initial_balance
 
-    account = self.env['saving_account'].search_read([('id','=',account_id.id)])
-    # form_data = self.read()[0] if self.read() else {
-    #     'date_from': from_date,
-    #     'date_to': to_date,
-    #     'account_id': [account_id.id, account_id.name]
-    # }
-    form_data =  {
-        'date_from': from_date,
-        'date_to': to_date,
-        'account_id': [account_id.id, account_id.name]
+    for entry in entries:
+      if entry['entry_type'] in {'withdraw', 'deposit', 'credit_interest'}:
+        amount = entry['amount']
+
+        if entry['entry_type'] == 'withdraw':
+          running_balance -= amount
+        else:
+          running_balance += amount
+
+        total_values[entry['entry_type']] += amount
+
+        entry['amount'] = truncate_number(amount, 2)
+        entry['balance'] = truncate_number(running_balance, 2)
+
+    account = self.env['saving_account'].search_read([
+      ('id', '=', account_id.id)
+    ])
+
+    form_data = {
+      'date_from': from_date,
+      'date_to': to_date,
+      'account_id': [account_id.id, account_id.name],
     }
-    
 
     data = {
-        'form': form_data,
-        'entry': entries,
-        'account_no': account[0]['account_no_signed'],
-        'total_withdraw': truncate_number(total_values['withdraw'], 2),
-        'total_deposit': truncate_number(total_values['deposit'], 2),
-        'total_interest': truncate_number(total_values['credit_interest'], 2),
+      'form': form_data,
+      'entry': entries,
+      'account_no': account[0]['account_no_signed'] if account else account_id.account_no,
+      'total_withdraw': truncate_number(total_values['withdraw'], 2),
+      'total_deposit': truncate_number(total_values['deposit'], 2),
+      'total_interest': truncate_number(total_values['credit_interest'], 2),
     }
 
     return data
 
-
   def action_print_report(self):
     data = self.generate_report()
-    return self.env.ref('saving_account.action_term_individual_account_report').report_action(self, data=data)
+
+    return self.env.ref(
+      'saving_account.action_term_individual_account_report'
+    ).report_action(self, data=data)
 
   def action_send_email(self):
+    self.ensure_one()
+
+    if not self.account_id:
+      raise UserError(_("Please select an account before sending email."))
+
     data = self.generate_report()
-    try:
-      report_id = self.env.ref('saving_account.action_term_individual_account_report')._render(self.ids, data=data)
-    except Exception as e:
-      print(e)
-    report_b64 = base64.b64encode(report_id[0])
+
+    report = self.env.ref(
+      'saving_account.action_term_individual_account_report'
+    )
+
+    pdf_content, report_type = report._render_qweb_pdf(
+      report.report_name,
+      res_ids=self.ids,
+      data=data
+    )
+
+    report_b64 = base64.b64encode(pdf_content)
+
     now = fields.Datetime.today().strftime('%Y%m%d')
     report_name = now + '_' + str(self.account_id.account_no) + '_term_individual_account.pdf'
-    
-    # create email attachment
+
     attachment = self.env['ir.attachment'].create({
-            'name': report_name,
-            'type': 'binary',
-            'datas': report_b64,
-            'store_fname': report_name,
-            'mimetype': 'application/x-pdf'
-        })
+      'name': report_name,
+      'type': 'binary',
+      'datas': report_b64,
+      'mimetype': 'application/pdf',
+    })
 
-    # find email to send to
-    email_to_send = self.env['email_setup'].search([], limit=1, order='create_date desc').email_to
-    email_values = {'email_to': email_to_send}
-    print("Sending email to", email_to_send)
+    email_to_send = self.email_to or self.env['email_setup'].search(
+      [],
+      limit=1,
+      order='create_date desc'
+    ).email_to
 
-    # send email template
-    report_template_id = self.env.ref('saving_account.mail_template_term_individual_account')
-    report_template_id.attachment_ids = [(6, 0, [attachment.id])]
+    if not email_to_send:
+      return {
+        'type': 'ir.actions.client',
+        'tag': 'display_notification',
+        'params': {
+          'title': _('Warning'),
+          'message': 'No email recipient configured.',
+          'sticky': True,
+        }
+      }
+
+    email_values = {
+      'email_to': email_to_send,
+      'attachment_ids': [(4, attachment.id)],
+    }
+
+    _logger.info(
+      "Sending term individual account email for account %s to %s",
+      self.account_id.account_no,
+      email_to_send
+    )
+
+    report_template_id = self.env.ref(
+      'saving_account.mail_template_term_individual_account'
+    )
+
     try:
-      # send confirmation message when successful
-      ### CHATGPT used 'with_context' method to add data dictionary to email template's context (easier to access data)
-      report_template_id.send_mail(self.id, email_values=email_values, force_send=True)
+      report_template_id.send_mail(
+        self.id,
+        email_values=email_values,
+        force_send=True
+      )
+
       return {
         'type': 'ir.actions.client',
         'tag': 'display_notification',
@@ -163,98 +192,133 @@ class TermIndividualAccountWizard(models.TransientModel):
           'sticky': True,
         }
       }
-    except:
-      # send warning message when fail
+
+    except Exception as e:
+      _logger.exception(
+        "Term individual account email send failed: %s",
+        e
+      )
+
       return {
-          'type': 'ir.actions.client',
-          'tag': 'display_notification',
-          'params': {
-            'title': _('Warning'),
-            'message': 'Email failed to send',
-            'sticky': True,
-          }
+        'type': 'ir.actions.client',
+        'tag': 'display_notification',
+        'params': {
+          'title': _('Warning'),
+          'message': 'Email failed to send: %s' % str(e),
+          'sticky': True,
+        }
       }
 
   def get_all_reports(self, account_ids):
     reports = []
+
     for account_id in account_ids:
       data = self.generate_report(account_id=account_id)
       reports.append(data)
-    return reports
 
+    return reports
 
   def get_all_pdfs(self, reports, report_id_ref):
     pdfs = []
-    for report in reports:
+
+    for report_data in reports:
       try:
-        report_id = report_id_ref._render(self.ids, data=report)
+        pdf_content, report_type = report_id_ref._render_qweb_pdf(
+          report_id_ref.report_name,
+          res_ids=self.ids,
+          data=report_data
+        )
+
+        report_b64 = base64.b64encode(pdf_content)
+        pdfs.append(report_b64)
+
       except Exception as e:
-          print(e)
-          continue
-      report_b64 = base64.b64encode(report_id[0])
-      pdfs.append(report_b64)
+        _logger.exception(
+          "Failed to generate individual account PDF: %s",
+          e
+        )
+        continue
+
     return pdfs
 
-
   def action_send_all_emails(self):
-    _logger.info('logger this is a test message')
+    self.ensure_one()
+
+    _logger.info("Start sending all term individual account emails")
+
     account_ids = self.env['saving_account'].search([])
-    report_id_ref = self.env.ref('saving_account.action_term_individual_account_report')
-    email_to_send = self.env['email_setup'].search([], limit=1, order='create_date desc').email_to
-    report_template_id = self.env.ref('saving_account.mail_template_term_individual_account')
 
-    all_reports = self.get_all_reports(account_ids=account_ids)
+    email_to_send = self.email_to or self.env['email_setup'].search(
+      [],
+      limit=1,
+      order='create_date desc'
+    ).email_to
 
-    for i in range(len(all_reports)):
+    if not email_to_send:
+      raise UserError(_("No email recipient configured."))
+
+    sent_count = 0
+    failed_count = 0
+
+    for account in account_ids:
       try:
-        report_id = report_id_ref._render(self.ids, data=all_reports[i])
-      except Exception as e:
-          print(e)
-          continue
-      report_b64 = base64.b64encode(report_id[0])
-      now = fields.Datetime.today().strftime('%Y%m%d')
-      report_name = now + '_' + str(account_ids[i].account_no) + '_term_individual_account.pdf'
-        # create email attachment
-      attachment = self.env['ir.attachment'].create({
-            'name': report_name,
-            'type': 'binary',
-            'datas': report_b64,
-            'store_fname': report_name,
-            'mimetype': 'application/x-pdf'
+        wizard = self.create({
+          'account_id': account.id,
+          'date_from': self.date_from or find_date_from(),
+          'date_to': self.date_to or fields.Date.today(),
+          'email_to': email_to_send,
         })
-      print("attachment made", report_name)
-      email_values = {'email_to': email_to_send}
-      print("Sending email to", email_to_send)
-      report_template_id = self.env.ref('saving_account.mail_template_term_individual_account')
-      report_template_id.attachment_ids = [(6, 0, [attachment.id])]
-      try:
-          report_template_id.send_mail(self.id, email_values=email_values)
-          print("Sent email to", email_to_send)
-      except:
-            # send warning message when fail
-          print("Email failed to send")
-    return True
 
+        wizard.action_send_email()
+        sent_count += 1
 
+        _logger.info(
+          "Sent term individual account email for account %s",
+          account.account_no
+        )
 
-    
+      except Exception as e:
+        failed_count += 1
 
+        _logger.exception(
+          "Failed to send term individual account email for account %s: %s",
+          account.account_no,
+          e
+        )
 
+        continue
+
+    return {
+      'type': 'ir.actions.client',
+      'tag': 'display_notification',
+      'params': {
+        'title': _('Completed'),
+        'message': 'Emails sent: %s, failed: %s' % (sent_count, failed_count),
+        'sticky': True,
+      }
+    }
 
   @api.model
   def _cron_send_email(self):
     try:
-      print("Sending scheduled email...")
-      self.action_send_all_emails()
-    except:
-      return {
-          'type': 'ir.actions.client',
-          'tag': 'display_notification',
-          'params': {
-            'title': _('Warning'),
-            'message': 'Email failed to send',
-            'sticky': True,
-          }
-      }
+      _logger.info("Sending scheduled term individual account emails...")
 
+      setup = self.env['email_setup'].search(
+        [],
+        limit=1,
+        order='create_date desc'
+      )
 
+      wizard = self.create({
+        'date_from': find_date_from(),
+        'date_to': fields.Date.today(),
+        'email_to': setup.email_to if setup else False,
+      })
+
+      wizard.action_send_all_emails()
+
+    except Exception as e:
+      _logger.exception(
+        "Scheduled term individual account email failed: %s",
+        e
+      )
